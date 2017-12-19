b1937ecfe26a8de6e7187744c04607967bcb13c6
Brad <mrwilson1@comcast.net>
2017-12-18 19:01:25 -0500

    First commit

---
+++ application/config/globals.php
@@ -0,0 +1,6 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+// https://www.formget.com/codeigniter-global-variable/
+
+$config['van'] = "VanHorn";
+$config['bos'] = "Bostick";
